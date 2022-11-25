e0ea742f89821df7d72f88adeb6ac7431dd3001e
laurent larfa <laurent.larfa@gmail.com>
2022-11-25 11:52:14 +0100

    review & show 1version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :butlers, through: :bookings
+  has_many :reviewtable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
---
+++ test/controllers/reviewtables_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ReviewtablesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
